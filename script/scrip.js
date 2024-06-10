f5474c07969f409acdc0d92b3ccd720e7043ccbd
Matthias Fütterer <mazzefm@gmail.com>
2024-06-10 14:42:52 +0200

    💄 change colour scheme

--- script/scrip.js
+++ script/scrip.js
@@ -1,37 +1,37 @@
 const ctx = document.getElementById("myChart");
 
 const chart = new Chart(ctx, {
-    type: "pie",
-    data: {
-        labels: ["Ihr Anteil", "Transaktionsgebühren"],
-        //main color: 002a5b
-        //accent color: 47b5f1
-        datasets: [
-            {
-                label: "von 1000 Euro",
-                data: [99.6, 0.4],
-                backgroundColor: ["#ff9900", "#ff0066"],
-                label: "Euro",
-                borderWidth: 0,
-                fontSize: 40,
-            },
-        ],
+  type: "pie",
+  data: {
+    labels: ["Ihr Anteil", "Transaktionsgebühren"],
+    //main color: 002a5b
+    //accent color: 47b5f1
+    datasets: [
+      {
+        label: "von 1000 Euro",
+        data: [99.6, 0.4],
+        backgroundColor: ["#20948b", "#8fb"],
+        label: "Euro",
+        borderWidth: 0,
+        fontSize: 40,
+      },
+    ],
+  },
+  options: {
+    plugins: {
+      tooltip: {
+        enabled: false,
+      },
+      border: {
+        display: false,
+      },
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Chart.js Pie Chart",
+      },
     },
-    options: {
-        plugins: {
-            tooltip: {
-                enabled: false,
-            },
-            border: {
-                display: false,
-            },
-            legend: {
-                position: "top",
-            },
-            title: {
-                display: false,
-                text: "Chart.js Pie Chart",
-            },
-        },
-    },
+  },
 });
